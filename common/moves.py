1fb27ca96650e114a191eed0120e288f0907ea6a
Jerome Acosta <gerome654@hotmail.com>
2023-10-31 21:21:19 -0400

    debugging wtf is tile[0]

--- common/moves.py
+++ common/moves.py
@@ -5,6 +5,7 @@ from data.board import Board
 moves = ["up", "down", "left", "right"]
 
 def simulate_move(direction: str, head):
+        new_head = np.array([])
         match direction:
             case 'up':
                 new_head = np.array([head[0], head[1] + 1])
@@ -19,11 +20,11 @@ def simulate_move(direction: str, head):
 def get_possible_moves(snake: Snake, board: Board):
     possible_moves = np.array([])
     head = snake.get_head()
-    neck = snake.get_neck()
 
     for m in moves:
          new_head = simulate_move(m, head)
          
+         print(f"\n\n\nNEW HEAD: {new_head}\n\n\n")
         # don't hit wall
          if new_head[0] == board.width or new_head[1] == board.height:
               continue
